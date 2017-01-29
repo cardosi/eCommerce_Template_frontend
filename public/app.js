ee98f6c612fd664c7a768e22afb49dc534347ee7
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-01-29 09:32:54 -0800

    images clickable

--- public/app.js
+++ public/app.js
@@ -45,47 +45,47 @@ app.controller('controller5', ['$routeParams', function($routeParams) {
 app.controller('controller6', function() {
 
 });
-
-
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $locationProvider.html5mode({ enabled: true });
-
-  $routeProvider.when('/logIn', {
-    templateUrl: 'partials/partial1.html',
-    controller: 'controller1',
-    controllerAs: 'ctrl1'
-  });
-
-  $routeProvider.when('/register', {
-    templateUrl: 'partials/partial2.html',
-    controller: 'controller2',
-    controllerAs: 'ctrl2'
-  });
-
-  $routeProvider.when('/cart/:id', {
-    templateUrl: 'partials/partial3.html',
-    controller: 'controller3',
-    controllerAs: 'ctrl3'
-  });
-
-  $routeProvider.when('/account/:id', {
-    templateUrl: 'partials/partial4.html',
-    controller: 'controller4',
-    controllerAs: 'ctrl4'
-  });
-
-  $routeProvider.when('/products/:id', {
-    templateUrl: 'partials/partial5.html',
-    controller: 'controller5',
-    controllerAs: 'ctrl5'
-  });
-
-  $routeProvider.when('/info', {
-    templateUrl: 'partials/partial6',
-    controller: 'controller6',
-    controllerAs: 'ctrl6'
-  });
-  $routeProvider.otherwise({
-    redirectTo: '/'
-  });
-}]);
+//
+//
+// app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+//   $locationProvider.html5mode({ enabled: true });
+//
+//   $routeProvider.when('/logIn', {
+//     templateUrl: 'partials/partial1.html',
+//     controller: 'controller1',
+//     controllerAs: 'ctrl1'
+//   });
+//
+//   $routeProvider.when('/register', {
+//     templateUrl: 'partials/partial2.html',
+//     controller: 'controller2',
+//     controllerAs: 'ctrl2'
+//   });
+//
+//   $routeProvider.when('/cart/:id', {
+//     templateUrl: 'partials/partial3.html',
+//     controller: 'controller3',
+//     controllerAs: 'ctrl3'
+//   });
+//
+//   $routeProvider.when('/account/:id', {
+//     templateUrl: 'partials/partial4.html',
+//     controller: 'controller4',
+//     controllerAs: 'ctrl4'
+//   });
+//
+//   $routeProvider.when('/products/:id', {
+//     templateUrl: 'partials/partial5.html',
+//     controller: 'controller5',
+//     controllerAs: 'ctrl5'
+//   });
+//
+//   $routeProvider.when('/info', {
+//     templateUrl: 'partials/partial6',
+//     controller: 'controller6',
+//     controllerAs: 'ctrl6'
+//   });
+//   $routeProvider.otherwise({
+//     redirectTo: '/'
+//   });
+// }]);
