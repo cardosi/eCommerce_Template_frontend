480f1ad2f9f7a757212d0554e81f283e2a1d9746
CCAZ88 <camcardosi@mac.com>
2017-01-31 08:39:49 -0700

    have user auth functioning

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,10 @@ app.controller('mainController', ['$http', function($http) {
   this.message = "connected!"
   this.products = [];
   this.productsloaded = false;
+  this.url = 'http://localhost:3000';
+  this.loggedIn = false;
 
-
+//CALL TO GET PRODUCTS - This should run on page load
   $http({
     method: 'GET',
     url: 'http://localhost:3000/products'
@@ -17,6 +19,54 @@ app.controller('mainController', ['$http', function($http) {
       console.log(response);
       this.productsloaded = true;
     }.bind(this));
+
+//CALL TO LOG IN - adds token to localStorage and changes Log In button in nav
+  this.login = function(userPass) {
+    console.log(userPass)
+    
+
+    $http({
+      method: 'POST',
+      url: this.url + '/users/login',
+      data: {user: {username: userPass.username, password: userPass.password}},
+    }).then(function(response){
+      console.log(response);
+      this.loggedIn = true;
+      this.userNow = response.data.user.username;
+      localStorage.setItem('token', JSON.stringify(response.data.token));
+
+
+      }.bind(this));
+    }
+
+//LOG OUT FUNCTION - removes token from local storage and resets Log In button
+  this.logOut = function(){
+    localStorage.removeItem('token');
+    this.loggedIn = false;
+    this.userNow = undefined;
+    userPass.username = '';
+    userPass.password = '';
+  }
+
+  this.register = function(regForm){
+    console.log(regForm.username);
+    console.log(regForm.password);
+    $http({
+      method: 'POST',
+      url: this.url + '/users',
+      data: {user: {username: regForm.username, password: regForm.password, email: regForm.email, name: regForm.name, address: regForm.address, city: regForm.city, state: regForm.state, zip: regFrom.zip}}
+    }).then(
+      function(response){
+        console.log(response);
+
+
+      }.bind(this)
+    );
+  }
+
+
+
+
 
 
 
